6b699eadbeda41a6495a22f6ebd8ff9e1e713fea
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-06-07 23:48:31 -0600

    Add hero and search partials

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,24 @@
+<section
+class="relative h-72 bg-primary flex flex-col justify-center align-center text-center space-y-4 mb-4"
+>
+<div
+    class="absolute top-0 left-0 w-full h-full opacity-5 bg-repeat bg-center mt-1"
+    style="background-image: url('images/BridgeWork.png')"
+></div>
+
+<div class="z-10">
+    <h1 class="text-6xl font-bold uppercase text-white">
+        Work<span class="text-secondary">Bridge</span>
+    </h1>
+    <p class="text-2xl text-gray-200 font-bold my-4">
+        Find or post jobs & projects
+    </p>
+    <div>
+        <a
+            href="register.html"
+            class="inline-block border-2 border-white bg-secondary text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+            >Sign Up to List a Job</a
+        >
+    </div>
+</div>
+</section>
