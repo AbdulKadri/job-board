c914ada70aa2a42cca45f1db0aca13ea3ced7c8f
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-06-08 02:18:01 -0600

    Add filter by tags or by search

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,15 +7,13 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         return view('listings.index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get(),
         ]);
     }
 
-    public function show(Listing $listing)
-    {
+    public function show(Listing $listing) {
         return view('listings.show', [
             'listing' => $listing,
         ]);
