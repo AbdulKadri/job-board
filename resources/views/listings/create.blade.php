fcaf1cef045e67905bda4bff2e4b0dd180c9fb95
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-06-08 22:13:18 -0600

    Change back button href

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -139,7 +139,7 @@
                 >
                     Create Job
                 </button>
-                <a href="/" class="text-black ml-4"> Back </a>
+                <a href="{{ url()->previous() }}" class="text-black ml-4"> Back </a>
             </div>
         </form>
     </x-card>
